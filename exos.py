fbf31b65dc4d563296019d7e81e4e81bdfd169be
Jeliiiii <elacroix@gaming.tech>
2022-11-07 18:19:52 +0100

    maj exos

--- exos.py
+++ exos.py
@@ -4,11 +4,14 @@ s = 1250
 e = 10 
 rh = 230
 
+assertionFinale = (((365 * 3) / (24 - (16 - 8))) * (rh) > r) == (e * s < r)
+
 part1 = ((365 * 3) / (24 - (16 - 8)) * (rh) > r) #t
 part2 = (e * s < r) #f
 
-assertionFinale = ( (365 * 3) / (24 - (16 - 8)) * (rh) > r) == (e * s < r)
-assertionFinale = part1 == part2
+assertionFinale2 = ((365 * 3) / (4 - (12 - 8)) * (rh) > r) == (e * s < r)
 
+part12 = ((365 * 3) / (4 - (12 - 8)) * (rh) > r) #f
+part22 = (e * s < r) #t
 
 # FIN
